390a06db9014cb4934dfddc15662c83d33387a08
kadulko <114679506+kadulko@users.noreply.github.com>
2023-06-28 15:38:20 +0200

    Close Modal on ESC added

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,10 +1,25 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import css from './Modal.module.css';
 
 const Modal = props => {
+  const handleKeyDown = ev => {
+    if (ev.keyCode === 27) {
+      props.onClick();
+    }
+  };
+
+  const ref = useRef(null);
+  useEffect(() => ref.current.focus(), []);
+
   return (
-    <div className={css.Overlay} onClick={props.onClick}>
+    <div
+      ref={ref}
+      className={css.Overlay}
+      onClick={props.onClick}
+      tabIndex={-1}
+      onKeyDown={handleKeyDown}
+    >
       <div className={css.Modal}>
         <img src={props.imgURL} alt="" />
       </div>
